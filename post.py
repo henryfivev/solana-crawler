49f3ddf0f04e3d5f7b1a630c1aabd50474dc6fc3
henry <773542531@qq.com>
2024-09-19 14:33:41 +0800

    [init] 已可以根据签名获取对应交易

---
+++ post.py
@@ -0,0 +1,62 @@
+import requests
+
+url = 'https://api.mainnet-beta.solana.com'
+
+# 标签数据有误，需要大小写敏感
+phishing_address = [
+    "DadEgL8xv3jB48UqEbC5y34zyMGBi8sH7r1oRn5oKukJ",
+    "13qESscnJbEZjsUi4fv47nvxMcHjp4VNRapSuog1wYus",
+    "CzXA1gwnbEByQfvNN5DbYZRLxxdDUGUgqCjTJrEBys1x",
+    "48mpgkYNJuJhuFt6uk3N7erEVn2GXpFtStAhSK49Rx71",
+    "HQwqACbdUqrcbUmbv53B72HJKuQZ6XeSG1zb9H1rbzn3"
+]
+
+def getSigForAddr(addr):
+    headers = {
+        'Content-Type': 'application/json',
+        # 'Authorization': ''
+    }
+    data = {
+        'jsonrpc': '2.0',
+        'id': 1,
+        'method': "getSignaturesForAddress",
+        "params": [
+            addr,
+            {
+                "limit": 10
+            }
+        ]
+    }
+    response = requests.post(url, json=data, headers=headers)
+
+    # print(response.status_code)
+    # print(response.json())
+    return response.json()
+
+def getSolTransaction(signature):
+    headers = {
+        'Content-Type': 'application/json',
+        # 'Authorization': ''
+    }
+    data = {
+        'jsonrpc': '2.0',
+        'id': 1,
+        'method': "getTransaction",
+        "params": [
+            signature,
+            "json"
+        ]
+    }
+    response = requests.post(url, json=data, headers=headers)
+
+    # print(response.status_code)
+    # print(response.json())
+    return response.json()
+
+if __name__ == "__main__":
+    for address in phishing_address:
+        sig_response_json = getSigForAddr(address)
+        signatures = [entry['signature'] for entry in sig_response_json['result']]
+        for signature in signatures:
+            trans_response_json = getSolTransaction(signature)
+            print(trans_response_json, '\n')
